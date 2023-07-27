39d00714d55e3cbb88a1d8aa9cc2005479ef2079
Yassine Sabir <s.yassine@hotmail.fr>
2023-07-27 14:16:22 +0200

    Goerli contract

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -12,10 +12,8 @@ module.exports = {
       url: "http://127.0.0.1:9545"
     },
     goerli: {
-      //url: "https://eth-goerli.g.alchemy.com/v2/Tq__YuHi-Fguvz3vmXoj9Vk-qSNXprqd",
       url: process.env.GOERLI_URL,
       accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
-      //accounts: [`0xad24224f680c56d5cd87b06892661b8cf6b0dcdedf905d16d8c8ff1bed466f41`],
     }
   }
 };
